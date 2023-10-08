497db7bc25c65906c430be83e42056420a938e06
Valentin387 <valentin.valencia@utp.edu.co>
2023-10-08 18:54:28 -0500

    added listAdmins endpoint

--- airline-backend/src/main/java/com/laboratory/airlinebackend/controller/AdminController.java
+++ airline-backend/src/main/java/com/laboratory/airlinebackend/controller/AdminController.java
@@ -1,4 +1,35 @@
 package com.laboratory.airlinebackend.controller;
 
+import com.laboratory.airlinebackend.model.User;
+import com.laboratory.airlinebackend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+@RestController
+@RequestMapping("/api/admin")
+//@CrossOrigin("http://localhost:5173")
+@RequiredArgsConstructor
 public class AdminController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/listAdmins")
+    public ResponseEntity<?> getAllAdministrators() {
+        List<User> admins = userRepository.findAll()
+                .stream()
+                .filter(user -> user.getRole() == 2)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(admins);
+    }
+
+
 }
