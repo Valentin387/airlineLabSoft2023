2653180b15b47b8d6200ffb223b8d1a50683fb92
Valentin387 <valentin.valencia@utp.edu.co>
2023-10-16 18:13:12 -0500

    Revisión hecha, limpié el código un poco
    --backend--
    /emailChecking and /newAdmin
    quitar los strong
    
    /register
    RegisterRequest and AuthenticationService
    poner los demás atributos
    
    --frontend--
    /register
    poner los demás atributos:
    DNI,
    email,
    password,
    firstName,
    lastName,
    birthday,
    birthPlace,
    billingAddress,
    gender,
    username,
    profileImage,
    subscribedToFeed
    
    --Preguntar a Brayan cuál era el endpoint en el postman en el cual teníamos que colocar password =""
    --
    
    **Llevar una USB con el JDK 20 para instalarlo en los compus de la sala

--- airline-frontend/src/services/authenticationService/registerService.js
+++ airline-frontend/src/services/authenticationService/registerService.js
@@ -1,8 +1,30 @@
 import axiosInstance from '../axiosInstance.js';
 
 
-const register = (firstName, lastName, email, password) => {
-  const credentials = { firstName, lastName, email, password };
+const register = (DNI,
+                  email,
+                  password,
+                  firstName,
+                  lastName,
+                  birthday,
+                  birthPlace,
+                  billingAddress,
+                  gender,
+                  username,
+                  profileImage,
+                  subscribedToFeed) => {
+  const credentials = { DNI,
+                  email,
+                  password,
+                  firstName,
+                  lastName,
+                  birthday,
+                  birthPlace,
+                  billingAddress,
+                  gender,
+                  username,
+                  profileImage,
+                  subscribedToFeed };
   return axiosInstance.post(`auth/register`, credentials);
 };
 
