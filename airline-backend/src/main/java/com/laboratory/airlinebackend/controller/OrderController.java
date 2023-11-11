8c2cb04f070f11b239916a11d5193797037dd518
Valentin387 <valentin.valencia@utp.edu.co>
2023-11-10 21:21:12 -0500

    added findPassenger by DNI endpoint

--- airline-backend/src/main/java/com/laboratory/airlinebackend/controller/OrderController.java
+++ airline-backend/src/main/java/com/laboratory/airlinebackend/controller/OrderController.java
@@ -1,5 +1,9 @@
 package com.laboratory.airlinebackend.controller;
+import com.laboratory.airlinebackend.repository.FlightRepository;
+import com.laboratory.airlinebackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -9,5 +13,38 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class OrderController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/findPassenger/{DNI}")
+    public ResponseEntity<?> findPassenger(@PathVariable String DNI){
+        try{
+            var passenger = userRepository.findByDNI(DNI);
+            return ResponseEntity.ok(passenger);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("That passenger is not a registered user " + e.getMessage());
+        }
+    }
+
+    /*
+    @PostMapping("/purchase")
+    public ResponseEntity<?> purchase(
+            @RequestBody RegisterRequestFlight requestNewFlight
+    ){
+        try{
+            //System.out.println(requestNewFlight);
+            var flight = Flight.builder()
+                    .flightDate(requestNewFlight.getFlightDate())
+                    .origin(requestNewFlight.getOrigin())
+                    .destination(requestNewFlight.getDestination())
+                    .costByPerson(requestNewFlight.getCostByPerson())
+                    .build();
+            flightRepository.save(flight);
+            return ResponseEntity.ok("Flight created successfully");
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Error creating flight " + e.getMessage());
+        }
+    }
+    */
 
 }
