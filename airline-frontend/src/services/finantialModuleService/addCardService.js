aca37dc22d3ddef268476de0142e3bac187eef6a
Valentin387 <valentin.valencia@utp.edu.co>
2023-11-17 12:25:54 -0500

    I just specified userID in AddCart service

--- airline-frontend/src/services/finantialModuleService/addCardService.js
+++ airline-frontend/src/services/finantialModuleService/addCardService.js
@@ -1,8 +1,8 @@
 import axiosInstance from '../axiosInstance.js';
               
 
-const addCard = (id, balance, type, number, name, expirationDate, cvc) => {
-  const credentials = { id, balance, type, number, name, expirationDate, cvc };
+const addCard = (userID, balance, type, number, name, expirationDate, cvc) => {
+  const credentials = { userID, balance, type, number, name, expirationDate, cvc };
   return axiosInstance.post(`financial-module/add/${id}`, credentials);
 };
 
